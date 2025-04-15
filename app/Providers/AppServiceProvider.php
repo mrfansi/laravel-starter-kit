cea845f7b247e32a18f106e7301d15622e411f05
Muhammad Irfan <mrfansi@outlook.com>
2025-04-15 22:39:02 +0700

    init

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('viewPulse', function ($user) {
+            return Str::endsWith($user->email, ['@viding.co', '@viding.id']);
+        });
     }
 }
