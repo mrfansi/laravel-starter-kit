048d2801725ad991b46f4439a19417455e4523a1
Muhammad Irfan <mrfansi@outlook.com>
2025-04-16 16:58:47 +0700

    init

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\VoltServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
 ];
