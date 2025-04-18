7a03d5c010e9ec7c7f0e5d1196b26ad054d9f3bf
Muhammad Irfan <mrfansi@outlook.com>
2025-04-18 12:16:29 +0700

    Add Instagram OAuth configuration to services config

--- config/services.php
+++ config/services.php
@@ -45,5 +45,10 @@ return [
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
         'redirect' => env('FACEBOOK_REDIRECT_URI'),
     ],
+    'instagram' => [
+        'client_id' => env('INSTAGRAM_CLIENT_ID'),
+        'client_secret' => env('INSTAGRAM_CLIENT_SECRET'),
+        'redirect' => env('INSTAGRAM_REDIRECT_URI'),
+    ],
 
 ];
